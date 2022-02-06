725f3f053be095478cd21ac5b6b28fb94166a0b6
Dev-Elie <eochieng9448@gmail.com>
2022-02-06 08:50:24 +0300

    ✅ Added tests

--- app/config.py
+++ app/config.py
@@ -6,9 +6,6 @@ class Settings(BaseSettings):
     database_password: str
     database_name : str
     database_username: str
-    secret_key : str
-    algorithm : str
-    access_token_expire_minutes : int
 
     class Config:
         env_file = ".env"
